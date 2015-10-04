f7fd072be6d2501bf0c1ed17dafd4ebc73f84d97
yuzhangiot <yuzhangiot@gmail.com>
2015-10-04 10:03:37 +0800

    redo figone.py

--- python/figone.py
+++ python/figone.py
@@ -15,32 +15,36 @@ except ImportError:
     # python 2
     from urllib import urlencode
 #convert bmp to jpg
-home = "/Users/joseph_zhang/"
-txtfilename = "python/data/cool4data.txt"
+home = "/Users/joseph_zhang/ether/sketch/gitdataprocess/"
+bmpnum = "1"
+# txtfilename = "python/data/cool4data.txt"
 
 def processData(datapath):
 	img = Image(filename = datapath)
 	img.save(filename = home + "ether/test/book.jpg")
 
-def drawPic(mt):
+def drawPic(smer,pi,cool4):
 	figure(figsize=(8,6), dpi=80)
 
 	subplot(1,1,1)
 
-	X = np.linspace(1, 100, 100,endpoint=True)
+	X = np.linspace(1, 50, 50,endpoint=True)
 	# C,S = np.cos(X), np.sin(X)
 
-	plot(X, mt, color="blue", linewidth=1.0, linestyle="-")
+	plot(X, smer, color="blue", linewidth=1.0, linestyle="-", label="SMER")
+	plot(X, pi, color="green", linewidth=1.0, linestyle=":", label="RaspberryPi")
+	plot(X, cool4, color="red", linewidth=1.0, linestyle="--", label="Linux Server")
 
 	# plot(X, S, color="green", linewidth=1.0, linestyle="-")
 
-	xlim(0,100.0)
+	xlim(0,50.0)
 
-	xticks(np.linspace(0,100,11,endpoint=True))
+	xticks(np.linspace(0,50,6,endpoint=True))
 
-	ylim(0.030,0.040)
+	ylim(-10,190)
 
-	yticks(np.linspace(0.000,0.10,11,endpoint=True))
+	yticks(np.linspace(-10.000,190,21,endpoint=True))
+	legend(loc='upper left')
 
 	# savefig("exercice_2.png",dpi=72)
 
@@ -66,15 +70,39 @@ def storeData():
 		file_object.write('%s\n' % item)
 	file_object.close()
 
-def readData():
+def readData(filename):
 	result=[]
-	with open(txtfilename,'r') as f:
+	with open(filename,'r') as f:
 		for line in f:
 			result.append(map(float,line.split(',')))
 	return result
 
+def myfilter(numlist):
+	result = []
+	for item in numlist:
+		if (item[0]>112):
+			continue
+		else:
+			result.append(item)
+	return result
+
+smerfile = home + "python/data/singleSMERdata_" + bmpnum + ".txt"
+smerdata = readData(smerfile)
+pifile = home + "python/data/pidata_" + bmpnum + ".txt"
+pidata = readData(pifile)
+cool4file = home + "python/data/cool4data_" + bmpnum + ".txt"
+cool4data = readData(cool4file)
+
+
+smerdatafilter = myfilter(smerdata)
+pidatafilter = pidata[:50]
+cool4datafilter = cool4data[:50]
+
+drawPic(smerdatafilter,pidatafilter,cool4datafilter)
+
+
 # datapath = home + "ether/test/book.jpg"
-storeData()
+
 # processData(datapath)
 
 
